1e0c2450627db828e3a525091d32e057a673f50d
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2020-03-05 21:37:40 +0200

    Basic routing configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
+import { Typography, Box } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <Router>
+            <Box p={3}>
+                <Link to="/one">
+                    <Typography variant="h4" align="center">
+                        A React boilerplate
+                    </Typography>
+                </Link>
+                <Typography variant="h4" align="center">
+                    A React boilerplate
+                </Typography>
+                <Box display="flex" flexDirection="row" justifyContent="center">
+                    {/* <Link href="/one">One</Link>
+                    <Box p={2} />
+                    <Link href="/two">Two</Link>
+                    <Box p={2} />
+                    <Link href="/three">Three</Link> */}
+                </Box>
+            </Box>
+            <Switch>
+                <Route path="/one">
+                    <Typography variant="h6" align="center">
+                        Page one
+                    </Typography>
+                </Route>
+                <Route path="/two">
+                    <Typography variant="h6" align="center">
+                        Page two
+                    </Typography>
+                </Route>
+                <Route path="/three">
+                    <Typography variant="h6" align="center">
+                        Page three
+                    </Typography>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
+        <CssBaseline />
         <App />
     </ThemeProvider>,
     document.getElementById('root')
